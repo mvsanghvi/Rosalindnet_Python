fe81845deda8b9b83a4b402150a7502c135773f3
mvsanghvi <mevsanghvi@gmail.com>
2023-08-23 22:53:09 -0500

    Alternatives

--- 2) Variables.py
+++ 2) Variables.py
@@ -1,3 +1,23 @@
 [a,b]= [] #insert intergers here
 n=(a**2 + b**2)
 print(n)
+
+# Alternative #1 (recommended)
+import os
+
+a = os.getenv('ENVIRONMENT_VARIABLE_A')
+B = os.getenv('ENVIRONMENT_VARIABLE_B')
+
+def squared(a, b):
+    return a**2 + b**2
+
+
+# Alternative #2
+def squared(a, b):
+    return a**2 + b**2
+
+print('Enter value for variable a:')
+a = int(input())
+print('Enter value for variable b:')
+b = int(input())
+print(squared(a,b))
