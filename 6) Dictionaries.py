2c748c3ea9f571ae6d9119c0eb655be931a8ed60
mvsanghvi <mevsanghvi@gmail.com>
2023-09-20 17:07:09 -0500

    Another attempt

--- 6) Dictionaries.py
+++ 6) Dictionaries.py
@@ -1,10 +1,9 @@
-def Convert(string):
-    li = list(string.split(" "))
-    return li
- 
- 
-# Driver code
-str1 = "Geeks for Geeks"
-print(Convert(str1))
-
 with open('rosalind_ini6.txt', 'r') as f:
+    lis = [line.split(" ") for line in f.readlines()]
+    print(lis)
+    sortedout= lis.sort()
+    print(sortedout)
+    for key, value in sortedout.items():
+        print(key)
+        print (value)
+    
