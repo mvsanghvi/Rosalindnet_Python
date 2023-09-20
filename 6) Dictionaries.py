cf6024495b57106346fe3d67260e331fc84b2485
mvsanghvi <mevsanghvi@gmail.com>
2023-09-20 16:31:58 -0500

    Initial Commit

--- 6) Dictionaries.py
+++ 6) Dictionaries.py
@@ -1,1 +1,10 @@
+def Convert(string):
+    li = list(string.split(" "))
+    return li
+ 
+ 
+# Driver code
+str1 = "Geeks for Geeks"
+print(Convert(str1))
+
 with open('rosalind_ini6.txt', 'r') as f:
