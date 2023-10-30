71c6040001ecf61dac18d127e6e77cc363b59b6b
mvsanghvi <mevsanghvi@gmail.com>
2023-10-30 16:46:34 -0500

    Reattempt 1 edit

--- 6) Dictionaries.py
+++ 6) Dictionaries.py
@@ -4,17 +4,19 @@ with open('rosalind_ini6.txt', 'r') as f:
     # print(lis)
     # sortedout= lis.sort()
     # print(sortedout)
-    # for key, value in sortedout.items():
-    #     print(key)
-    #     print (value)
     lns= f.readlines()
     print(lns)
     print(type(lns))
-    lis = lns[0].split(" ")
-    print(lis)
-    lis.sort()
-    print(lis)
-    print(Counter(lis))
+    for word in lns[0].split(" "):
+        print(word)
+    # print(lis)
+    # lis.sort()
+    # print(lis)
+    # for key, value in sortedout.items():
+    #     print(key)
+    #     print (value)
+
+    # print(Counter(lis))
 
     # cnt = f.split( )
     # print(cnt)
