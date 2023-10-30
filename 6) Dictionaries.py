c507421464a7a97c337ab758b6a8dc0310a90bb1
mvsanghvi <mevsanghvi@gmail.com>
2023-10-29 22:43:24 -0500

    More attempts

--- 6) Dictionaries.py
+++ 6) Dictionaries.py
@@ -1,9 +1,19 @@
 with open('rosalind_ini6.txt', 'r') as f:
-    lis = [line.split(" ") for line in f.readlines()]
+    # lis = [line.split(" ") for line in f.readlines()]
+    # print(lis)
+    # sortedout= lis.sort()
+    # print(sortedout)
+    # for key, value in sortedout.items():
+    #     print(key)
+    #     print (value)
+    lns= f.readlines()
+    print(lns)
+    print(type(lns))
+    lis = lns[0].split(" ")
     print(lis)
-    sortedout= lis.sort()
-    print(sortedout)
-    for key, value in sortedout.items():
-        print(key)
-        print (value)
-    
+    lis.sort()
+    print(lis)
+
+    # cnt = f.split( )
+    # print(cnt)
+#parce sentence into a list, count occurances
