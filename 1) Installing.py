d1639a51960bde1dcba36da1b278b779670211bd
mvsanghvi <mevsanghvi@gmail.com>
2025-02-27 21:57:08 -0600

    Documenting

--- 1) Installing.py
+++ 1) Installing.py
@@ -1,3 +1,8 @@
+# Problem
+
+# After downloading and installing Python, type "import this" into the Python command line and see what happens. 
+# Then, click the "Download dataset" button below and copy the Zen of Python into the space provided.
+
 import this
 
 #typed "python installing.py" into command line and hit enter
--- 2) Variables.py
+++ 2) Variables.py
@@ -1,3 +1,9 @@
+# Problem:
+
+# Given: Two positive integers a and b, each less than 1000.
+# Return: The integer corresponding to the square of the hypotenuse of the right triangle whose legs have lengths a and b.
+
+
 [a,b]= [] #insert intergers here
 n=(a**2 + b**2)
 print(n)
