0a962000aca607af7fcf8f66bf9387dc0d99ab94
mvsanghvi <mevsanghvi@gmail.com>
2025-03-26 22:12:59 -0500

    Passed Import Test

--- Bioinformatics Stronghold/2) Second Nucleic Acid.py
+++ Bioinformatics Stronghold/2) Second Nucleic Acid.py
@@ -1,10 +1,12 @@
-dataset = "GATGGAACTTGACTACGTAAATT"
 rna = ""
 
-for i in dataset:
-    if i == "T":
-        rna += "U"
-    else:
-        rna += str(i)
+with open('rosalind_ini8.txt', 'r') as f:
+    h= f.readlines()
+    h=h[0]
+    print(h)
+    for i in h:
+        if i == "T":
+            rna += "U"
+        else:
+            rna += str(i)
 print(rna)
-
